6064edc8feac0861e3bc87febcbd48b89f7660aa
Anish Anushang <anish.anushang@outlook.com>
2019-10-17 04:15:03 +0530

    practice bt

---
+++ practice/practice/src/main/java/com/student/practice/done/practiceBT/typeX/AllPermutation.java
@@ -0,0 +1,45 @@
+package com.student.practice.done.practiceBT.typeX;
+
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+public class AllPermutation {
+
+    public static void main(String[] args) {
+        AllPermutation app = new AllPermutation();
+        Integer[] arr = {1, 2, 3};
+        Consumer<Integer[]> consumer = ans -> System.out.println(Arrays.toString(ans));
+        app.permute(arr, 0, consumer);
+    }
+
+    private void permute(Integer[] arr, int currIndex, Consumer<Integer[]> consumer) {
+        // current node = current state of (arr, currIndex)
+        if (currIndex == arr.length - 1) {
+            // current node is a leaf node
+            consumer.accept(arr);
+        } else {
+            // current node is not a leaf node
+            // one by one go to all child node of current node as
+            // go to one child node, dfs, come back to current node, go to other child node
+            for (int nextIndex = currIndex; nextIndex <= arr.length - 1; nextIndex++) {
+                // current node = current state of (arr, currIndex)
+                Integer next = arr[nextIndex];
+                Integer curr = arr[currIndex];
+                // change state of (arr, currentIndex) => go to a child node from current node
+                arr[currIndex] = next;
+                arr[nextIndex] = curr;
+                currIndex++;
+                // child node = current state of (arr, currIndex)
+                // dfs
+                permute(arr, currIndex, consumer);
+                // change back state of (arr, currIndex) => come back to current node from child node
+                currIndex--;
+                arr[currIndex] = curr;
+                arr[nextIndex] = next;
+                // current node = current state of (arr, currIndex)
+            }
+        }
+        // current node = current state of (arr, currIndex)
+    }
+
+}
